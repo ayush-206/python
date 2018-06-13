a04eb7e83c5f29739651c184501af50e2931d66f
ayush-206 <as6316206@gmail.com>
2018-06-13 12:36:44 +0530

    new file added

---
+++ assigment2.py
@@ -0,0 +1,11 @@
+#placing the values with placeholders
+course="python"
+fees=5000
+s="acadview"
+print('s:%s\nfees:%d'%(s,course,fees))
+
+#find error and solve this
+#print('%s"%d')%(_,_)
+name="tony stark"
+salary=1000000
+print('name:%s,salary:%d'%(name,salary))
