72ec817e1d68379646a1beccc82dc82f51c270a7
ayush-206 <as6316206@gmail.com>
2018-06-14 11:34:51 +0530

    file is added

---
+++ assignment1.py
@@ -0,0 +1,16 @@
+#Git Commands
+git config --global user.name"your name"
+git config --global user.email"email"
+git init
+git add fie name #this is for selective approch
+git add . #this is for adding all file
+git status 
+git commit -m"message"
+git add origin master "link from our git repository"
+git push origin master
+
+#git updation
+git add .
+git commit -m"message"
+git status
+git push origin master
