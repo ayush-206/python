dc4397573c67916c8a00eb7129e83ce28b885d08
ayush-206 <as6316206@gmail.com>
2018-06-15 16:10:13 +0530

    modified file

---
+++ assignmentt2.py
@@ -0,0 +1,26 @@
+#program to print anything on screen
+print("hello friends")
+
+#join two strings using '+' operator
+print("Acad"+"View")
+
+#take the input of 3 variable x,y,z. print their value on screen
+x=4
+y=2.10
+z=3.2
+print(x,y,z)
+
+#print"let's get started" on screen
+print("let's get started")
+
+#placing the values with placeholders
+course="python"
+fees=5000
+s="acadview"
+print("%s  %s  %d " %(s,course,fees) )
+
+#find error and solve this
+#print('%s"%d')%(_,_)
+name="tony stark"
+salary=1000000
+print('name:%s,salary:%d'%(name,salary))
